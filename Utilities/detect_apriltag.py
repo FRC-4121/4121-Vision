044dd433f495787ba488e43e3b23040488ccd019
Matthew Cornell <mcornell400708@gmail.com>
2024-01-03 15:27:30 +0000

    *agonized screaming*

--- Utilities/detect_apriltag.py
+++ Utilities/detect_apriltag.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
 # import the necessary packages
-import apriltag
+import pyapriltags as apriltag
 import argparse
 import cv2
 
+params = (8.25, 8.25, 320, 240)
+tag_size = 0.16
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image containing AprilTag")
-args = vars(ap.parse_args())
+group = ap.add_mutually_exclusive_group(required=True)
+group.add_argument("-i", "--image", metavar="PATH", help="path to input image containing AprilTag")
+group.add_argument("-c", "--cam", metavar="ID", nargs="?", type=int, default=0, help="use camera frame")
+args = ap.parse_args()
 
-# load the input image and convert it to grayscale
-print("[INFO] loading image...")
-image = cv2.imread(args["image"])
+image = cv2.imread(args) if args.image is not None else cv2.VideoCapture(args.cam if args.cam is not None else 0).read()[1]
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# define the AprilTags detector options and then detect the AprilTags
-# in the input image
-print("[INFO] detecting AprilTags...")
-options = apriltag.DetectorOptions(families="tag36h11")
-detector = apriltag.Detector(options)
-results = detector.detect(gray)
-print("[INFO] {} total AprilTags detected".format(len(results)))
+detector = apriltag.Detector(families="tag36h11")
+results = detector.detect(gray, True, params, tag_size)
 
 # loop over the AprilTag detection results
 for r in results:
@@ -46,11 +43,12 @@ for r in results:
 	cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
 	
 	# draw the tag family on the image
-	tagFamily = r.tag_family.decode("utf-8")
-	cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+	tagId = r.tag_id
+	cv2.putText(image, str(tagId), (ptA[0], ptA[1] - 15),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-	print("[INFO] tag family: {}".format(tagFamily))
+	
 	
 # show the output image after AprilTag detection
 cv2.imshow("Image", image)
+
 cv2.waitKey(0)
--- tests/AprilTag.py
+++ tests/AprilTag.py
@@ -1,63 +1,118 @@
-import apriltag
+import pyapriltags
 import cv2
 import time
+import numpy as np
+from threading import Thread
+import os
 
-camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_BRIGHTNESS, 50)  # Brightness
-camera.set(cv2.CAP_PROP_EXPOSURE, 100)  # Exposure
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-options = apriltag.DetectorOptions(families="tag36h11")
-detector = apriltag.Detector(options)
-lastGrabbed = True
+params = (8.25, 8.25, 320, 240)
+tag_size = 0.16
 
-height = cv2.getTextSize("0", cv2.FONT_HERSHEY_SIMPLEX, 0.25, 1)[0][1] + 2
+frames = []
+kill = False
 
-while True:
-    
-    grabbed, frame = camera.read()
+def find_cams(port: int):
+    file = f"/sys/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb1/1-1/1-1.{port}/1-1.{port}:1.0/video4linux"
+    if os.path.exists(file):
+        files = [int(x[5:]) for x in os.listdir(file) if x[:5] == "video" and x[5:].isnumeric()]
+        files.sort()
+        if len(files) > 0:
+            return files[0]
 
-    if not grabbed:
-        if lastGrabbed:
-            print("Failed to grab frame")
-            lastGrabbed = False
-        # continue
-
-    lastGrabbed = True
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    results = detector.detect(gray)
-
-    for r in results:
-        (ptA, ptB, ptC, ptD) = r.corners
-        ptB = (int(ptB[0]), int(ptB[1]))
-        ptC = (int(ptC[0]), int(ptC[1]))
-        ptD = (int(ptD[0]), int(ptD[1]))
-        ptA = (int(ptA[0]), int(ptA[1]))
-        # draw the bounding box of the AprilTag detection
-        cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
-        cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
-        cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
-        cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
-
-        # draw the tag family on the image
-        cv2.putText(frame, str(r.tag_id), (ptA[0], ptA[1] - 15),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+class Runner:
+    def __init__(self, idx, port=None, *, id=None):
+        self.idx = idx
+        self.port = port
+        self.id = id if id is not None else find_cams(port)
+        self.detector = pyapriltags.Detector(families="tag36h11")
+        self.camera = cv2.VideoCapture(self.id)
+        self.camera.set(cv2.CAP_PROP_BRIGHTNESS, 50)  # Brightness
+        self.camera.set(cv2.CAP_PROP_EXPOSURE, 100)  # Exposure
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.lastGrabbed = True
+        self.lastTime = time.monotonic()
+        self.frame = np.zeros((480, 620, 3), dtype=np.uint8)
         
-        pose = detector.detection_pose(r, (8.25, 8.25, 320, 240), 0.16)[0]
+        
+    def tick(self):
+        print(f"tick for {self.id}")
+        grabbed, frame = self.camera.read()
+        
+        if not grabbed:
+            if self.lastGrabbed:
+                print("Failed to grab frame")
+                self.lastGrabbed = False
 
-        cv2.putText(frame, "{: >4.2} {: >4.2} {: >4.2}".format(*pose[0]), (ptB[0] + 15, ptB[1]),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0), 1)
+        self.lastGrabbed = True
 
-        cv2.putText(frame, "{: >4.2} {: >4.2} {: >4.2}".format(*pose[1]), (ptB[0] + 15, ptB[1] + height),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0), 1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        results = self.detector.detect(gray, True, params, tag_size)
 
-        cv2.putText(frame, "{: >4.2} {: >4.2} {: >4.2}".format(*pose[2]), (ptB[0] + 15, ptB[1] + height * 2),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0), 1)
+        for r in results:
+            (ptA, ptB, ptC, ptD) = r.corners
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+            # draw the bounding box of the AprilTag detection
+            cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
 
-    cv2.imshow("Tags", frame)
+            # draw the tag family on the image
+            cv2.putText(frame, str(r.tag_id), (ptA[0], ptA[1] - 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    if cv2.waitKey(1) == 27:
-        cv2.destroyAllWindows()
-        break
+            cv2.putText(frame, "{: >4.2f} {: >4.2f} {: >4.2f}".format(*r.pose_t.flatten()), (ptB[0] + 15, ptB[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 255), 1)
+
+
+        current = time.monotonic()
+        cv2.putText(frame, str(1 // (current - self.lastTime)) + " FPS", (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        self.lastTime = current
+
+        self.frame = frame
+    
+    def launch(self):
+        while not kill:
+            self.tick()
+
+    def show(self):
+        cv2.imshow("Camera " + str(self.port if self.port is not None else self.id), self.frame)
+
+cams = [0, 2]
+
+
+runners = [Runner(idx, id=port) for (idx, port) in enumerate(cams)]
+
+def run_multi():
+    threads = [Thread(target=Runner.launch, name="camera" + str(this.port), args=(this,)) for this in runners]
+
+    for thread in threads:
+        thread.start()
+
+    while True:
+        for cam in runners:
+            cam.show()
+        
+        if cv2.waitKey(10) == 27:
+            cv2.destroyAllWindows()
+            kill = True
+            break
+
+def run_single():
+    while True:
+        print("main tick")
+        for cam in runners:
+            cam.tick()
+            cam.show()
+        
+        if cv2.waitKey(1) == 27:
+            cv2.destroyAllWindows()
+            kill = True
+            break
+        print("end")
+
+run_single()
