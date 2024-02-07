86d6beba307e475ec95b89d04a8e28aa23434bef
Victor Jon Griswold <victor.j.griswold@gmail.com>
2024-02-06 23:06:25 -0500

    Simple SysV-init daemon script for 4121-Vision pipeline.
    Installation instructions are commented within the script.
    ToDo:  auto-detect within 'raspberrypi4.py' that it _is_ being run
           as a daemon (likely through an environment variable which
           populates the 'videoTesting' global).

---
+++ Utilities/4121-vision.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#   This script enables the FRC Team 4121 Vision processing to start upon boot of the Raspberry Pi 4.
+#   Install this script by issuing these commands:
+#     sudo cp 4121-vision.sh /etc/init.d
+#     cd /etc/rc5.d
+#     sudo ln -s ../init.d/4121-vision.sh S024121-vision
+#     sudo ln -s ../init.d/4121-vision.sh K024121-vision
+
+. /lib/lsb/init-functions
+
+PIDFILE="/var/run/4121-vision.pid"
+
+if [ "$TEAM4121HOME" = "" ]; then
+  TEAM4121HOME="/home/team4121/4121-Vision"
+fi
+export TEAM4121HOME
+
+STATUS=0
+
+case "$1" in
+  start)
+    start-stop-daemon --start --pidfile $PIDFILE --make-pidfile --background --exec ${TEAM4121HOME}/run/raspberrypi4.py
+	  STATUS=$?
+	  if [ $STATUS != 0 ]; then
+	    log_end_msg $STATUS
+    fi
+    ;;
+  stop)
+    start-stop-daemon --stop --pidfile $PIDFILE --remove-pidfile --oknodo
+	  STATUS=$?
+	  if [ $STATUS != 0 ]; then
+	    log_end_msg $STATUS
+    fi
+    ;;
+  *)
+    echo "Usage: $0 start" >&2
+    exit 3
+    ;;
+esac
+exit $STATUS
