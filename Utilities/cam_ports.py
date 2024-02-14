f93713cd567f15b505e3d6e74372f2cf09d6a850
Matthew Cornell <mcornell400708@gmail.com>
2024-02-14 16:53:04 -0500

    Change camera brightness

--- Utilities/cam_ports.py
+++ Utilities/cam_ports.py
@@ -3,8 +3,9 @@ import os
 
 # How does this work? I have no idea!
 def find_cams(port: int):
-    # Pi 4
+    port = int(port)
     port4 = [2, 1, 4, 3][port]
+    # Pi 4
     file = f"/sys/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb1/1-1/1-1.{port4}/1-1.{port4}:1.0/video4linux"
     if os.path.exists(file):
         files = [
@@ -17,7 +18,9 @@ def find_cams(port: int):
             return files[0]
     # Pi 5
     port5 = [0, 1, 3, 2][port]
-    file = f"/sys/devices/platform/axi/1000120000.pcie/1f00{port % 2 + 2}00000.usb/xhci-hcd.{port5 % 2}/usb{port5 % 2 * 2 + 1}/{port5 % 2 * 2 + 1}-{port5 // 2 + 1}/{port5 % 2 * 2 + 1}-{port5 // 2 + 1}:1.0/video4linux"
+    file = "/sys/devices/platform/axi/1000120000.pcie/1f00{0}00000.usb/xhci-hcd.{1}/usb{2}/{2}-{3}/{2}-{3}:1.0/video4linux".format(
+        port5 % 2 + 2, port5 % 2, port5 % 2 * 2 + 1, port5 // 2 + 1
+    )
     if os.path.exists(file):
         files = [
             int(x[5:])
--- Utilities/cfg_cams.sh
+++ Utilities/cfg_cams.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for cam in 0 1 2 3; do
-    v4l2-ctl -d /dev/video$cam -c auto_exposure=1 2> /dev/null
-    v4l2-ctl -d /dev/video$cam -c exposure_time_absolute=300 2> /dev/null
+for cam in 0 2; do
+    v4l2-ctl -d /dev/video$cam -c auto_exposure=1
+    v4l2-ctl -d /dev/video$cam -c exposure_time_absolute=300
 done
